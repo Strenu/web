97702df677cc2ec2ddee22029f3327be2ea12b2e
Julen <julenricocrespo@gmail.com>
2025-07-21 21:05:14 +0200

    [FIX] Widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:strenu_web/main.dart';
+import 'package:strenu_web/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
